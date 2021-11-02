edae7e3a5608dd8061ecedfbba2d13fd050f0a5a
arthurcassemiroo <arthurcassemiro.ac@gmail.com>
2021-11-02 01:22:52 -0300

    Static charts

--- frontend/src/components/NavBar/Footer/index.tsx
+++ frontend/src/components/NavBar/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/arthurcassemiroo" target="_blank" rel="noreferrer">Arthur Cassemiro</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/arthur_cassemiro.ig" target="_blank" rel="noreferrer">@arthur_cassemiro.ig</a></small></p>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@arthur_cassemiro.ig</a></small></p>
             </div>
         </footer>
     );
