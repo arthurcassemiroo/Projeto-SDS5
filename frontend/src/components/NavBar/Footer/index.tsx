4ac48d197fd57268e42efb6ec122bee9a6188c78
arthurcassemiroo <arthurcassemiro.ac@gmail.com>
2021-11-02 00:36:22 -0300

    Basic static components

---
+++ frontend/src/components/NavBar/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/arthurcassemiroo" target="_blank" rel="noreferrer">Arthur Cassemiro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/arthur_cassemiro.ig" target="_blank" rel="noreferrer">@arthur_cassemiro.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
