45f142b2638ec3b5ad1708240feed93031c2dd39
arthurcassemiroo <arthurcassemiro.ac@gmail.com>
2021-11-05 10:16:21 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,9 +1,37 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 function DonutChart() {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    // WRONG FORM
+    let chartData: ChartData ={labels: [], series: []};
+
+    //WRONG FORM
+    axios.get("${BASE_URL}/sales/amount-by-seller")
+        .then(Response => {
+            const data = Response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = {labels: myLabels, series: mySeries[]};
+            console.log(Response.data)
+        }
+
+
+
+
+
+
+
+
+
+    //const mockData = {
+    //    series: [477138, 499928, 444867, 220426, 473088],
+    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //}
 
     const options = {
         legend: {
